370ee0422307d0764da25fb115fcd51a3b49fe43
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-02-17 22:40:36 +0100

    style code snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,18 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
+
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
