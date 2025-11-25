bc0d1a8824cb758127e4dedd09a9d0e5f61d9b8e
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-11-25 15:19:16 +0100

    update rss & npm packages outdated

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,20 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function get(context) {
+export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    trailingSlash: false,
     items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.id}/`,
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `id`
+      // This example assumes all posts are rendered as `/[id]` routes
+      link: `/${post.id}/`,
     })),
     stylesheet: "rss/styles.xsl",
   });
